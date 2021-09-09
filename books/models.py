0d36b953111704e84a9685c17eb8934739a79eea
traceannwhite <traceannwhite@gmail.com>
2021-09-09 12:04:46 -0500

    model updated

--- books/models.py
+++ books/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Books(models.Model):
-    image = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, blank=True, null=True)
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=30)
-    description = models.TextField(max_length=1000)
-    read = models.BooleanField(default=False)
+    description = models.TextField(max_length=1000,blank=True, null=True)
+    read = models.BooleanField(default=False, blank=True, null=True)
 
