1d46d30bfc422ba63cab5f962d0e39469669d4de
traceannwhite <traceannwhite@gmail.com>
2021-09-09 09:28:43 -0500

    m

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.apps.BooksConfig',
+    'books.apps.BookConfig',
     'rest_framework',
     'corsheaders'
 ]
